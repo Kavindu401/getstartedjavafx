95b36f0e109f5d74ffdaf86bdd22d4f185b7850d
Kavindu401 <kavinduperera2211@gmail.com>
2024-02-16 16:08:03 +0530

    information page

---
+++ src/main/java/org/example/signup/HelloController.java
@@ -0,0 +1,16 @@
+package org.example.signup;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
